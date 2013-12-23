1a776374815b4d9b3502c17db29e128990a05de7
Peter Wood <peter@alastria.net>
2013-12-23 21:34:06 +0000

    Basic Empty Mavenised Plugin.

---
+++ src/main/java/net/caldonia/bukkit/plugins/PLUGINNAME/Plugin.java
@@ -0,0 +1,11 @@
+package net.caldonia.bukkit.plugins.PLUGINNAME;
+
+public class Plugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+	}
+
+    @Override
+    public void onDisable() {
+	}	
+}
