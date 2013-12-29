eb3043104b475f50a36d17be471e9f60cd1c02a9
Peter Wood <peter@alastria.net>
2013-12-29 20:17:40 +0000

    Rename empty project into Chunk or No Chunk.

---
+++ src/main/java/net/caldonia/bukkit/plugins/chunkornochunk/ChunkOrNoChunk.java
@@ -0,0 +1,13 @@
+package net.caldonia.bukkit.plugins.chunkornochunk;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ChunkOrNoChunk extends JavaPlugin {
+    @Override
+    public void onEnable() {
+	}
+
+    @Override
+    public void onDisable() {
+	}	
+}
